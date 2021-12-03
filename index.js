b8338352c56a870a2bd1604980408f4f340e40e0
Ziapa <SmiT4eggg@bk.ru>
2021-12-03 11:52:36 +0300

    add gegUsers

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 const app = express()
 app.use(cors())
 
-const crm = new AmoCRM({
+export const crm = new AmoCRM({
     domain: 'ziapa',
     auth: {
         client_id: 'ea6e99be-6e54-401c-98b3-123169b5345c',
@@ -26,7 +26,7 @@ app.get("/", async (req, res) => {
 })
 
 app.get("/leads", async (req, res) => {
-    const response = await crm.request('GET', '/api/v4/leads');
+    const response = await crm.request('GET', `/api/v4/leads`);
 
     return res.json(response.data)
 })
@@ -39,6 +39,12 @@ app.get("/leads/:id", async (req, res) => {
 
 app.get("/contacts/", async (req, res) => {
     const response = await crm.request('GET', `/api/v4/contacts`);
+
+    return res.json(response.data)
+})
+
+app.get("/users/", async (req, res) => {
+    const response = await crm.request('GET', `/api/v4/users`);
 
     return res.json(response.data)
 })
@@ -58,6 +64,12 @@ app.get("/pipelines", async (req, res) => {
 
 app.get("/statuses/:id", async (req, res) => {
     const response = await crm.request('GET', `/api/v4/leads/pipelines/${req.params.id}/statuses`);
+
+    return res.json(response.data)
+})
+
+app.get("/statuses/:id:pipeline_id", async (req, res) => {
+    const response = await crm.request('GET', `/api/v4/leads/pipelines/${req.params.pipeline_id}/statuses/${req.params.id}`);
 
     return res.json(response.data)
 })
---
+++ routes/dealRoutes.js
@@ -0,0 +1,7 @@
+import {DealController} from "../controller/dealController";
+
+const Router = require("express")
+const router =  Router()
+
+
+module.exports = router
